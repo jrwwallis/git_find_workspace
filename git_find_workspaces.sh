a3dec2ed2c34b84d2677bf3d6a7a63669f0c920f
John Wallis <jwallis@zooxlabs.com>
2024-02-17 21:16:12 -0800

    linux working

---
+++ git_find_workspaces.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# detect if "-executable" test is supported by this `find`
+if find / -maxdepth 0 -executable >/dev/null 2>&1 ; then
+    # Probably GNU find
+    dirAccessible="-executable"
+else
+    # Probably BSD find
+    dirAccessible="-perm -g+x"
+fi
+
+# assume current directory, if none specified
+if [ "$1" = "" ]; then
+    wd=.
+else
+    wd="$1"
+fi
+
+# find predicates:
+#   don't traverse into other filesystems
+#   skip anything not a directory
+#   skip any non-accessible directory
+#   skip ..dotfiles, but allow ..
+#   skip .dotfiles, but allow ..
+#   git workspace found! print, then don't traverse deeper; continue
+
+find "${wd}" \
+    -mount \
+    ! -type d -prune -o \
+    ! ${dirAccessible} -prune -o \
+    -name '..?*' -prune -o \
+    -name '.[^.]*' -prune -o \
+    -execdir test -f '{}/.git/HEAD' \; -print -prune
